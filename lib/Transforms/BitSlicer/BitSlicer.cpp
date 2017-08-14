c5a0f21a7583bf29a2040e7db73d62279f759ee7
Paolo Savini <paolo.savini@embecosm.com>
2017-08-14 17:51:31 +0100

    changed some intrinsics, turned the BitSlicer pass into a ModulePass, added the mechanism for the deletion of the instructions to be transformed and added a basic parsing mechanism of the annotation for the orthogonalization

--- lib/Transforms/BitSlicer/BitSlicer.cpp
+++ lib/Transforms/BitSlicer/BitSlicer.cpp
@@ -1,6 +1,8 @@
 #include "llvm/Pass.h"
-#include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Constant.h"
 #include "llvm/IR/Instructions.h"
+#include "llvm/IR/IntrinsicInst.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/InstrTypes.h"
@@ -27,6 +29,12 @@
 using namespace llvm;
 
 std::vector<Instruction *> eraseList;
+std::vector<Instruction *> OrthEraseList;
+std::vector<BasicBlock *> OrthBBEraseList;
+std::vector<CallInst *> startSplitPoints;
+std::vector<CallInst *> endSplitPoints;
+std::vector<CallInst *> BitSliceAlloc;
+std::vector<StringRef> Descriptions;
 std::vector<StringRef> AllocOldNames;
 std::vector<AllocaInst *> AllocNewInstBuff;
 std::vector<StringRef> BitSlicedAllocNames;
@@ -34,7 +42,7 @@ std::vector<AllocaInst *> BitSlicedAllocInstBuff;
 
 
 
-bool BitSlice(CallInst *call, LLVMContext &Context){
+bool GetBitSlicedData(CallInst *call, LLVMContext &Context){
 	IRBuilder<> builder(call);
 		
 	Instruction *inputSize = cast<Instruction>(call->getArgOperand(0));
@@ -100,7 +108,7 @@ bool BitSlice(CallInst *call, LLVMContext &Context){
 }
 
 
-bool UnBitSlice(CallInst *call, LLVMContext &Context){
+bool GetUnBitSlicedData(CallInst *call, LLVMContext &Context){
 	IRBuilder<> builder(call);
 	int i, j, k;
 	
@@ -168,83 +176,286 @@ bool UnBitSlice(CallInst *call, LLVMContext &Context){
 }
 
 
-
-void EndOrthogonalTransformation(CallInst *call, 
-								 BasicBlock *startBlock, 
-								 BasicBlock *endBlock,
-								 Value *Description){
-	BasicBlock *bb;
-	std::vector<BasicBlock *> BlockList;
-	for (bb = startBlock; bb != endBlock; bb = bb->getUniqueSuccessor()) {
-		BlockList.push_back(bb);
-	}
-	auto CExtr = new CodeExtractor(ArrayRef <BasicBlock *>(BlockList));
-	Function *NewFunc = CExtr->extractCodeRegion();
+bool BitSlice(CallInst *call, LLVMContext &Context){
+	IRBuilder<> builder(call);
+/*	
+	BasicBlock *Pred = call->getParent();
+	BasicBlock *Succ = call->getParent()->splitBasicBlock(call, "for(alloca).end");
+	BasicBlock *New = BasicBlock::Create(Context, "for(alloca).cond");
+	New->insertInto(call->getFunction(), Succ);
+	Pred->getTerminator()->setSuccessor(0, New);
+*/		
+	Instruction *inputSize = cast<Instruction>(call->getArgOperand(0));
+	for(; !isa<AllocaInst>(inputSize); inputSize = cast<Instruction>(inputSize->getOperand(0)));
+	AllocaInst *oldAlloca = cast<AllocaInst>(inputSize);
+	AllocOldNames.push_back(oldAlloca->getName());
 	
-	//BasicBlock *entry = BasicBlock::Create(getGlobalContext(), "entry", NewFunc);
+	if(!isa<ArrayType>(oldAlloca->getAllocatedType())){
+		errs() << "ERROR: argument 1 not a static array\n";
+		return false;
+	}
+	uint64_t oldSize = cast<ArrayType>(oldAlloca->getAllocatedType())->getNumElements();
+	MDNode *MData = MDNode::get(Context, 
+								MDString::get(Context, "bit-sliced"));
+	oldAlloca->setMetadata("bit-sliced", MData);
+	//oldAlloca->replaceAllUsesWith(oldAlloca);
+	
+	Type *sliceTy = IntegerType::getInt32Ty(Context);
+	ArrayType *arrTy = ArrayType::get(sliceTy, (oldSize/32)*8);		//FIXME: need to make it type dependant.
+	AllocaInst *all = builder.CreateAlloca(arrTy, 0, "SLICES");
+	AllocNewInstBuff.push_back(all);
+	
+	int i, j;
+	
+	std::vector<Value *> IdxList;
+	Type *idxTy = IntegerType::getInt64Ty(Context);
+	Value *idxZero = ConstantInt::get(idxTy, 0);
+	Value *tmp;
+	Value *Byte;
+	Value *bitVal;
+	IdxList.push_back(idxZero);
+	IdxList.push_back(idxZero);
+	Value *sliceAddr;
+	int BitSizeOfInput = (oldSize/32)*8;
+	
+	for( i = 0; i < BitSizeOfInput; i++ ){
+		IdxList.at(1) = ConstantInt::get(idxTy, i);
+		sliceAddr = builder.CreateGEP(all, ArrayRef <Value *>(IdxList), "sliceAddr");
+		tmp = ConstantInt::get(sliceTy, 0);	
+		for( j = 0; j < 32; j++){				//FIXME: dependant on the target machine
+			IdxList.at(1) = ConstantInt::get(idxTy, j*BitSizeOfInput/8+i/8);
+			Byte = builder.CreateGEP(oldAlloca, ArrayRef <Value *>(IdxList), "Block");
+			Byte = builder.CreateLoad(Byte);
+
+			bitVal = builder.CreateZExt(Byte, sliceTy);			
+			bitVal = builder.CreateLShr(bitVal, ConstantInt::get(sliceTy, i%8));
+			bitVal = builder.CreateAnd(bitVal, ConstantInt::get(sliceTy, 1));
+			bitVal = builder.CreateShl(bitVal, ConstantInt::get(sliceTy, j));
+			tmp = builder.CreateOr(tmp, bitVal);
+		}
+		builder.CreateStore(tmp, sliceAddr);
+	}
+	
+	return true;
+}
+
+void OrthogonalTransformation(CallInst *call, StringRef Description){
+	//StringRef Description = cast<ConstantDataSequential>(cast<User>(cast<User>(call->getArgOperand(1))
+	//						->getOperand(0))->getOperand(0))->getAsCString();
+	errs() << Description << "\n";
+	BasicBlock *OrthBlock = call->getParent();
+	StringRef op;
+	std::vector<StringRef> tokens;
+	bool hasDestination, hasFirstOperand, hasSecondOperand, hasOp, preOp, postOp, end;
+	hasDestination = hasFirstOperand = hasSecondOperand = hasOp = preOp = postOp = end = false;
+	size_t i, pos;
+	for(i=0, pos=0; !end;){
+		pos = Description.find(":", i);
+		if(pos==0){
+			errs() << "error: description can't begin with colon ':'\n";
+			return;
+		}
+		if(pos==StringRef::npos){
+	//	errs() << "end: i, pos: " << i << " " << pos << "\n";
+			if(!i){	
+				errs() << "error: expected colon ':' to separate the tokens of the description\n";
+				return;
+			}else{
+				pos = Description.size();
+				
+			//	errs() << "end: i, pos: " << i << " " << pos << " " << Description.back() << "\n";
+				tokens.push_back(Description.substr(i, pos-i));
+				end = true;
+				continue;
+			}
+		}
+		if((pos-i) == 0){
+				i++;
+			//if(hasDestination && hasFirstOperand){
+				if(!preOp){
+					preOp = true;
+			//	if(!postOp){
+					pos = Description.find(":", i);
+					op = Description.substr(i, pos-i);
+					hasOp = true;
+					//postOp = true;
+					i = pos+1;
+					pos = Description.find(":", i);
+				}if((pos-i) == 0){
+					i++;
+					postOp = true;
+				}else{
+					//errs() << "i, pos: " << i << " " << pos << "\n";
+					errs() << "error: found too many uses of '::' separator\n";
+					return;
+				}
+			/*}else{
+				errs() << "error: ";
+				if(hasFirstOperand){
+					errs() << "no destination\n";
+					return;
+				}
+				
+				if(hasDestination){
+					errs() << "no left operand\n";
+					return;
+				}
+				
+				errs() << "no destination and no left operand\n";
+				return;
+			}*/
+		}else{
+			tokens.push_back(Description.substr(i, pos-i));
+		//	errs() << Description.substr(i, pos-i) << " - " << i << "\n";
+			i = pos+1;
+		}
+	}
+	
+	for(auto s : tokens){
+		errs() << s << "\n";
+	}
+	
+}
+
+/*
+void EndOrthogonalTransformation(BasicBlock *startBlock, 
+								 BasicBlock *endBlock,
+								 StringRef Description){
+errs() << __LINE__ << "\n";
+	
+	bool stop = false;
+	LoopInfoBase<BasicBlock, Loop> *CheckLoops = new LoopInfoBase<BasicBlock, Loop>();
+errs() << __LINE__ << "\n";
+	std::vector<BasicBlock *> BlockList;
+	BlockList.push_back(startBlock);
+	Function *Func = startBlock->getParent();
+	for (Function::iterator b = Func->begin(); startBlock != &BB; b++) {
+		BasicBlock &BB = *b;
+	}
+	
+	for (; BB != *endBlock; b++){
+		BB = *b;
+		BB.dump();
+		BlockList.push_back(&BB);
+	}
+*/	
+/*
+		if (CheckLoops->isLoopHeader(&BB))
+				errs() << "Looooop\n";
+errs() << __LINE__ << "\n";
+		const TerminatorInst *TInst = BB.getTerminator();
+		for (unsigned it = 0, NSucc = TInst->getNumSuccessors(); it < NSucc; ++it) {
+			BasicBlock *Succ = TInst->getSuccessor(it);
+			
+			BB.dump();
+			if (CheckLoops->isLoopHeader(Succ))
+				errs() << "Looooop\n";
+			if (Succ != endBlock)
+				BlockList.push_back(Succ);
+		}
+		
+		BB = *(TInst->getSuccessor(0));
+	}
+*/
+/*
+errs() << __LINE__ << "\n";	
+	auto CExtr = new CodeExtractor(ArrayRef <BasicBlock *>(BlockList));
+errs() << __LINE__ << "\n";	
+	Function *NewFunc = CExtr->extractCodeRegion();
+errs() << __LINE__ << "\n";	
+	//NewFunc->deleteBody();
+	
+	//BasicBlock *entry = BasicBlock::Create(call->getModule()->getContext(), "entry", NewFunc);
 	//IRBuilder<> builder(entry);
 	
 	
 	//for(; token = ParseDescription(Description);)
 }
-
+*/
 
 
 namespace{
 	
-	struct BitSlicer : public FunctionPass{
+	struct BitSlicer : public ModulePass{
 		static char ID;
 		//static int TYPE_OK;
 //		static int INSTR_TYPE;
 //		static int LAST_INSTR_TYPE;
 		static int done;
-		BitSlicer() : FunctionPass(ID) {}
+		BitSlicer() : ModulePass(ID) {}
 		
-		BasicBlock *orthStartBlock = nullptr;
+		BasicBlock *orthStartBlock = nullptr, *orthEndBlock = nullptr;
 		Value *orthDescription;
+		bool OrthErase = false;
+		bool OrthBBErase = false;
+		bool SameBlock = false;
 /*		std::vector<LoadInst *> LoadInstBuff;
 		std::vector<GetElementPtrInst *> GEPInstBuff;
 		
 		std::vector<StringRef> AllocNames;
 		std::vector<StringRef> LoadNames;
 		std::vector<StringRef> GEPNames;
-*/		
-		bool runOnFunction(Function &F){
+*/
+			
+		bool runOnModule(Module &M) override {
 		//	int i;
+		
+//		errs() << __LINE__ << "\n";
+		for(Function& F : M){
 			for(BasicBlock& B : F){
+				
+	//		B.dump();
 				for(Instruction& I : B){
+					if(OrthErase)
+						OrthEraseList.push_back(&I);
 					IRBuilder<> builder(&I);
 					if(auto *call = dyn_cast<CallInst>(&I)){
 						Function *Fn = call->getCalledFunction();
-						if(Fn && Fn->getIntrinsicID() == Intrinsic::bitslice_i32){
+						if(Fn && Fn->getIntrinsicID() == Intrinsic::getbitsliced_i32){
+							if(!GetBitSlicedData(call, I.getModule()->getContext())){
+								errs() << "bit-slicing failed\n";
+								}
+							eraseList.push_back(&I);
+							done = 1;
+							
+						}else if(Fn && Fn->getIntrinsicID() == Intrinsic::getunbitsliced_i32){
 					//		errs() << "args: \n" << call->getNumArgOperands() << "\n";
 							
+							if(!GetUnBitSlicedData(call, I.getModule()->getContext())){
+								errs() << "bit-slicing inversion failed\n";
+								}
+							eraseList.push_back(&I);
+							done = 1;
+						}else if(Fn && Fn->getIntrinsicID() == Intrinsic::bitslice_i32){
+					//		errs() << "args: \n" << call->getNumArgOperands() << "\n";
+							BitSliceAlloc.push_back(call);
 							if(!BitSlice(call, I.getModule()->getContext())){
-								errs() << "bitslicing failed\n";
-								}
+								errs() << "bit-slicing failed\n";
+							}
+							
 							eraseList.push_back(&I);
-							done = 1;
-							
-						}else if(Fn && Fn->getIntrinsicID() == Intrinsic::unbitslice_i32){
-					//		errs() << "args: \n" << call->getNumArgOperands() << "\n";
-							
-							if(!UnBitSlice(call, I.getModule()->getContext())){
-								errs() << "unbitslicing failed\n";
-								}
-							eraseList.push_back(&I);
-							done = 1;
-							
+							done = 1;	
 						}else if(Fn && Fn->getIntrinsicID() == Intrinsic::start_bitslice){
-							
-							orthStartBlock = B.splitBasicBlock(&I, "START_ORTHO");
-							orthDescription = call->getArgOperand(1);
-						//	StartOrthogonalTransformation(call, orthStartBlock);
-							
+							MDNode *MData = MDNode::get(I.getModule()->getContext(), 
+								MDString::get(I.getModule()->getContext(), "start-orthogonalization"));
+							call->setMetadata("start-orthogonalization", MData);
+							startSplitPoints.push_back(call);
+							Descriptions.push_back(cast<ConstantDataSequential>(cast<User>(cast<User>(call->getArgOperand(1))
+													->getOperand(0))->getOperand(0))->getAsCString());
+			//		errs() << __LINE__ << "\n";
+						
+						//	OrthogonalTransformation(call);
+							OrthErase = true;
+							eraseList.push_back(&I);				
 						}else if(Fn && Fn->getIntrinsicID() == Intrinsic::end_bitslice){
-							
-							EndOrthogonalTransformation(call, orthStartBlock, 
-														B.splitBasicBlock(&I, "END_ORTHO"),
-														orthDescription);
+							MDNode *MData = MDNode::get(I.getModule()->getContext(), 
+								MDString::get(I.getModule()->getContext(), "stop-orthogonalization"));
+							call->setMetadata("stop-orthogonalization", MData);
+							endSplitPoints.push_back(call);
+							OrthEraseList.pop_back();
+//					errs() << __LINE__ << "\n";
+							OrthErase = false;
+							eraseList.push_back(&I);
 						}
 					}
 					
@@ -472,17 +683,124 @@ namespace{
 						
 					done = 1;
 					}
+				}//I : B
+			}//B : F
+			
+			
+			
+			}//F : M
+		/*	
+			for(auto &SSP: startSplitPoints){
+				StringRef StartName = cast<ConstantDataSequential>(cast<User>(cast<User>(SSP->getArgOperand(0))
+															->getOperand(0))->getOperand(0))->getAsCString();
+				orthStartBlock = SSP->getParent()->splitBasicBlock(SSP, Twine(StartName));
+				
+				for(auto &ESP: endSplitPoints){
+					StringRef EndName = cast<ConstantDataSequential>(cast<User>(cast<User>(SSP->getArgOperand(0))
+															->getOperand(0))->getOperand(0))->getAsCString();
+					if(StartName.equals(EndName)){
+						EndName = StringRef(EndName.str()+"_END");
+						orthEndBlock = ESP->getParent()->splitBasicBlock(ESP, Twine(EndName));
+						StringRef Descr = cast<ConstantDataSequential>(cast<User>(cast<User>(SSP->getArgOperand(1))
+															->getOperand(0))->getOperand(0))->getAsCString();
+						errs() << Descr << "\n";
+						EndOrthogonalTransformation(orthStartBlock, orthEndBlock, Descr);
+					}
 				}
 			}
-			for(auto &EI: eraseList){
-				EI -> eraseFromParent();
+		*/
+			
+			
+			for(auto& ESP : endSplitPoints){
+				for(auto& SSP : startSplitPoints){
+					StringRef StartName = cast<ConstantDataSequential>(cast<User>(cast<User>(SSP->getArgOperand(0))
+												->getOperand(0))->getOperand(0))->getAsCString();
+						
+					StringRef EndName = cast<ConstantDataSequential>(cast<User>(cast<User>(ESP->getArgOperand(0))
+												->getOperand(0))->getOperand(0))->getAsCString();
+		//		errs() << __LINE__ << "\n";		
+					if(EndName.equals(StartName)){
+					//SSP->getParent()->dump();
+					//ESP->getParent()->dump();
+		//		errs() << __LINE__ << "\n";	
+						if(ESP->getParent() != SSP->getParent()){
+							BasicBlock *Pred = SSP->getParent();
+							orthStartBlock = SSP->getParent()->splitBasicBlock(SSP);
+							orthEndBlock = ESP->getParent()->splitBasicBlock(ESP);
+							Pred->getTerminator()->setSuccessor(0, orthEndBlock);
+		//			errs() << __LINE__ << "\n";
+						}
+					
+					}
+					
+				}
+			}	
+			
+			
+			
+			for(auto &OEI: OrthEraseList){
+				if(!OEI->use_empty())	
+					OEI->replaceAllUsesWith(UndefValue::get(OEI->getType()));
+				OEI -> eraseFromParent();
+			}
+		
+			for(Function& F : M){
+				for(BasicBlock& B : F){
+					if(OrthBBErase)
+							OrthBBEraseList.push_back(&B);
+					for(Instruction& I : B){
+						if(I.getMetadata("start-orthogonalization")){
+						//	OrthBBEraseList.push_back(&B);
+						//	OrthogonalTransformation(cast<CallInst>(&I));
+							OrthBBErase = true;
+						}
+						if(I.getMetadata("stop-orthogonalization")){
+							OrthBBEraseList.pop_back();
+							OrthBBErase = false;
+						}
+					}
+				}
 			}
 			
+			for(auto &EB: OrthBBEraseList){
+				EB -> eraseFromParent();
+			}
+			
+			for(auto& ESP : endSplitPoints){
+				StringRef EndName = cast<ConstantDataSequential>(cast<User>(cast<User>(ESP->getArgOperand(0))
+																->getOperand(0))->getOperand(0))->getAsCString();
+				for(auto& SSP : startSplitPoints){
+					StringRef StartName = cast<ConstantDataSequential>(cast<User>(cast<User>(SSP->getArgOperand(0))
+																	->getOperand(0))->getOperand(0))->getAsCString();
+					
+					if(EndName.equals(StartName)){
+						StringRef Descr = cast<ConstantDataSequential>(cast<User>(cast<User>(SSP->getArgOperand(1))
+																	->getOperand(0))->getOperand(0))->getAsCString();
+						OrthogonalTransformation(ESP, Descr);
+						SSP->getParent()->eraseFromParent();
+		//				errs() << __LINE__ << "\n";	
+					}
+				}
+			}
+	
+/*		
+			for(Function& F : M){
+				for(BasicBlock& B : F){
+					B.dump();
+				}
+			}
+*/	//errs() << __LINE__ << "\n";			
+			for(auto &EI: eraseList){
+				if(EI->getParent() != nullptr)
+					EI -> eraseFromParent();
+			}
+	//errs() << __LINE__ << "\n";			
 			if(done)
 				return true;
 			return false;
-		}	//runOnFunction
-	};	//class FunctionPass
+			
+		}	//runOnModule
+	};	//class ModulePass
 } //namespace
 
 char BitSlicer::ID = 0;
@@ -496,10 +814,12 @@ static void registerBitSlicerPass(const PassManagerBuilder &,
   PM.add(new BitSlicer());
 }
 static RegisterStandardPasses
-  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
+  RegisterMyPass(PassManagerBuilder::EP_ModuleOptimizerEarly,
 registerBitSlicerPass);
 
-
+static RegisterStandardPasses
+  RegisterMyPass0(PassManagerBuilder::EP_EnabledOnOptLevel0,
+registerBitSlicerPass);
 
 
 
